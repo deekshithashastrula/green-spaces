8429ff8461614429f61ef16b98ec1866dd2b4466
Deekshitha Shastrula <deekshitha.shastrula@gmail.com>
2020-06-30 10:54:53 +0530

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-    * Start Bootstrap - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
+    * Green Spaces - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
+    * Copyright 2013-2020 Green Spaces
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
     */
     (function($) {
